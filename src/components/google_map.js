161cea361b8a6b13811660262543b9dfcbd9da40
manish_tixdo <balyan05.manish@gmail.com>
2017-06-22 12:39:50 +0530

    chart and google map integration complete

---
+++ src/components/google_map.js
@@ -0,0 +1,21 @@
+/**
+ * Created by consultadd on 22/6/17.
+ */
+import React from 'react';
+import {GoogleMapLoader, GoogleMap} from 'react-google-maps'
+
+export default (props)=>{
+    return(
+        <GoogleMapLoader
+            containerElement={
+                <div style={{ height: `100%` }} />
+            }
+            googleMapElement={
+                <GoogleMap
+                defaultZoom={10}
+                defaultCenter={{ lat: props.lat, lng: props.lon }}/>
+                }
+        />
+    );
+}
+
--- src/containers/weatherlist.js
+++ src/containers/weatherlist.js
@@ -4,21 +4,28 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux';
 import Chart from "../components/chart";
+import _ from 'lodash'
+import GoogleMap from "../components/google_map";
+
 
 
 class WeatherList extends Component{
 
     renderWeather(cityData){
-        const temps = cityData.list.map(weather=>weather.main.temp);
+        const temps = _.map(cityData.list.map(weather=>weather.main.temp),(temp)=>temp-273.15);
+        const pressures = cityData.list.map(weather=>weather.main.pressure);
+        const humidities = cityData.list.map(weather=>weather.main.humidity);
+        const {lat,lon} = cityData.city.coord;
         console.log(temps);
         return (
             <tr key={cityData.city.id}>
                 <td>
-                    {cityData.city.name}
+                    <GoogleMap lat={lat} lon={lon}/>
+                    <h3>{cityData.city.name}</h3>
                 </td>
-                <td>
-                   <Chart data={temps} color='red'/>
-                </td>
+                <td><Chart data={temps} color='red' unit="C"/></td>
+                <td><Chart data={pressures} color='green' unit="hpa"/></td>
+                <td><Chart data={humidities} color='orange' unit="%"/></td>
             </tr>
         )
     }
@@ -29,9 +36,9 @@ class WeatherList extends Component{
                 <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temprature</th>
-                    <th>Presure</th>
-                    <th>Humidity</th>
+                    <th>Temprature (C)</th>
+                    <th>Presure (hpa)</th>
+                    <th>Humidity (%)</th>
                 </tr>
                 </thead>
                 <tbody>
